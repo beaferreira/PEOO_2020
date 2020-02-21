2cfaef85f8bb5d4b3354f530e7470ea32a738595
BEATRIZ FERREIRA DA SILVA <ferreira.b@academico.ifrn.edu.br>
2020-02-21 17:46:11 -0300

    exercicio01 lista 02

---
+++ ListaPOO02/Exercício01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite dois valores inteiros: ");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+
+            if (n1 > n2) Console.WriteLine($"Maior: {n1}");
+            else Console.WriteLine($"Maior: {n2}");
+            Console.ReadKey();
+            
+        }
+    }
+}
