f64e05a655fc2a6d01921162bda407582b6fc42b
Beatriz Ferreira da Silva <beatriz190703@hotmail.com>
2020-02-24 19:31:44 -0300

    Exercício 02

---
+++ ListaPOO02/Exercício 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite quatro valores inteiros: ");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+
+            int media = (a + b + c + d) / 4;
+            Console.WriteLine($"media = {media}");
+            
+            Console.WriteLine("Números menores que média: ");
+            if (a < media) Console.WriteLine($"{a}");
+            if (b < media) Console.WriteLine($"{b}");
+            if (c < media) Console.WriteLine($"{c}");
+            if (d < media) Console.WriteLine($"{d}");
+
+            Console.WriteLine($"Números maiores ou iguais à média: ");
+            if (a >= media) Console.WriteLine($"{a}");
+            if (b >= media) Console.WriteLine($"{b}");
+            if (c >= media) Console.WriteLine($"{c}");
+            if (d >= media) Console.WriteLine($"{d}");
+            Console.ReadKey();
+
+        }
+    }
+}
